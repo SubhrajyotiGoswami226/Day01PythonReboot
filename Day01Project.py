2abe9f46a12c2846fee8f6302523209f1535d9cb
SubhrajyotiGoswami226 <75923850+SubhrajyotiGoswami226@users.noreply.github.com>
2024-12-27 14:37:46 +0530

    Add files via upload

---
+++ Day01Project.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator! ")
+user_Name = input("Enter your name: ")
+user_City = input("Enter the name of the city your band is based: ")
+user_Food = input("Enter the name of the band's favourite food: ")
+user_Band = user_City + "'s " + user_Food
+print("Hey ", user_Name, "\nI think your band should be named as ", user_Band)
